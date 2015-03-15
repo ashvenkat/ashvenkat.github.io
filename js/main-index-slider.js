41aa5629d7cc3de484cec3dfd23f68ac4de412a2
ashvenkat <ashyvenk@gmail.com>
2015-03-15 11:49:28 +0530

    first commit

---
+++ js/main-index-slider.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('.intro-slider').owlCarousel({singleItem:true,pagenation:true,itemsDesktop:[1000,4],itemsDesktopSmall:[900,2],itemsTablet:[600,1],itemsMobile:false});});
